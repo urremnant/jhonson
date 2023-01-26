9e6718d9216c5f5dc447dd67d74127463779c170
juhee.kim <juhee.kim@theragenbio.com>
2023-01-26 18:20:33 +0900

    asfasfasf

---
+++ src/main/java/kkkim/com/juhee/JuheeApplication.java
@@ -0,0 +1,13 @@
+package kkkim.com.juhee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuheeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kkkim/com/juhee/JuheeApplicationTests.java
@@ -0,0 +1,13 @@
+package kkkim.com.juhee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
